42d7840a6899fff8b48b00711361ae84cc2a8b7a
ruvcreator <ruvenosuna09@gmail.com>
2026-06-05 11:48:14 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function showMission() {
+    document.getElementById("mission").style.display = "block";
+    document.getElementById("vision").style.display = "none";
+}
+
+function showVision() {
+    document.getElementById("vision").style.display = "block";
+    document.getElementById("mission").style.display = "none";
+}
+
+function hideAll() {
+    document.getElementById("mission").style.display = "none";
+    document.getElementById("vision").style.display = "none";
+}
